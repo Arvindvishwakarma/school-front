65afdb0a9dc6ccf5ce40c1e17484e40e7ec8152f
Arvind Vishwakarma <arvindvish2588@gmail.com>
2021-06-12 18:34:45 +0530

    almost front

--- src/components/admin/StudentList.js
+++ src/components/admin/StudentList.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Card, Button, Col, Row, Jumbotron, Modal, Table, Container } from 'react-bootstrap'
 import Spinner from '../Spinner'
-import { FaEye, FaSchool, FaTrash } from "react-icons/fa";
+import { FaEye, FaTrash, FaUserCircle } from "react-icons/fa";
 
 export default function StudentList() {
     const [spin, setSpin] = useState(false)
     const [spinDetail, setSpinDetail] = useState(false)
     const [spinDelete, setSpinDelete] = useState(false)
+
+    const [studentData, setStudentData] = useState([])
     const [schoolData, setSchoolData] = useState([])
-    const [singleSchoolData, setSingleSchoolData] = useState([])
+    const [singleStudentData, setSingleStudentData] = useState([])
     const [schoolId, setSchoolId] = useState('')
 
     const [show, setShow] = useState(false);
@@ -30,18 +32,20 @@ export default function StudentList() {
             query: `
                     query{
                         
-                        getSchool{
+                        getStudent{
                             id
-                            schoolName
-                            schoolCode
-                            schoolEmail
-                            schoolContact
-                            schoolAddress
-                            schoolLng
-                            schoolLat
-                            headName
-                            headEmail
-                            headContact 
+                            schoolId
+                            firstName
+                            lastName
+                            fatherName
+                            motherName
+                            dob
+                            gender
+                            catagory
+                            email
+                            contact
+                            class
+                            address
                           }
                     }
     
@@ -62,8 +66,8 @@ export default function StudentList() {
             return res.json()
         })
             .then(async (resData) => {
-                const fetchData = await resData.data.getSchool
-                setSchoolData(fetchData)
+                const fetchData = await resData.data.getStudent
+                setStudentData(fetchData)
                 setSpin(false)
             })
 
@@ -73,24 +77,26 @@ export default function StudentList() {
             })
     }
 
-    function fetchSingleSchoolData(sId) {
+    function fetchSingleStudentData(sId) {
         setSpinDetail(true)
         const requestBody = {
             query: `
                     query{
                         
-                        getSingleSchool(schoolId:"${sId}") {
+                        getSingleStudent(studentId:"${sId}"){
                             id
-                            schoolName
-                            schoolCode
-                            schoolEmail
-                            schoolContact
-                            schoolAddress
-                            schoolLng
-                            schoolLat
-                            headName
-                            headEmail
-                            headContact
+                            schoolId
+                            firstName
+                            lastName
+                            fatherName
+                            motherName
+                            dob
+                            gender
+                            catagory
+                            email
+                            contact
+                            class
+                            address
                           }
                     }
     
@@ -111,9 +117,55 @@ export default function StudentList() {
             return res.json()
         })
             .then(async (resData) => {
-                const fetchData = await resData.data.getSingleSchool
-                setSingleSchoolData(fetchData)
-                setSpinDetail(false)
+                const fetchData = await resData.data.getSingleStudent
+                setSingleStudentData(fetchData)
+
+                const requestBody = {
+                    query: `
+                            query{
+                                
+                                getSingleSchool(schoolId:"${fetchData.schoolId}") {
+                                    id
+                                    schoolName
+                                    schoolCode
+                                    schoolEmail
+                                    schoolContact
+                                    schoolAddress
+                                    schoolLng
+                                    schoolLat
+                                    headName
+                                    headEmail
+                                    headContact
+                                  }
+                            }
+            
+                        `
+                };
+
+
+                fetch('http://localhost:4000/graphql', {
+                    method: 'POST',
+                    body: JSON.stringify(requestBody),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => {
+                    if (res.status !== 200 && res.status !== 201) {
+                        throw new Error('Failed')
+                    }
+                    return res.json()
+                })
+                    .then(async (resData) => {
+                        const fetchData = await resData.data.getSingleSchool
+                        console.log("schoolData inside", fetchData)
+                        setSchoolData(fetchData)
+                        setSpinDetail(false)
+                    })
+
+                    .catch(err => {
+                        setSpinDetail(false)
+                        console.log(err)
+                    })
             })
 
             .catch(err => {
@@ -127,8 +179,8 @@ export default function StudentList() {
         const requestBody = {
             query: `
                     mutation{  
-                        deleteSchool(schoolId:"${sId}"){
-                            schoolName
+                        deleteStudent(studentId:"${sId}") {
+                            id
                           }
                     }
     
@@ -159,28 +211,32 @@ export default function StudentList() {
             })
     }
 
-    console.log(schoolData)
+    console.log("Student Data",studentData)
+    console.log("School Data",schoolData)
     return (
         <div>
             <Card className="schoolList">
                 <Card.Header>
-                    <Button className="mx-auto d-block" style={{ borderRadius: '0px' }}><Link to="/checkSchool"  style={{ textDecoration: 'none', color: 'white' }}>Add Student</Link></Button>
+                    <Button className="mx-auto d-block" style={{ borderRadius: '0px' }}><Link to="/checkSchool" style={{ textDecoration: 'none', color: 'white' }}>Add Student</Link></Button>
                 </Card.Header>
                 <Card.Body>
 
                     {
                         spin ? <center><Spinner /></center> :
 
-                            schoolData.map(data =>
+                            studentData.map(data =>
                                 <Jumbotron style={{ padding: '5px', marginBottom: '10px' }}>
                                     <Row>
-                                        <Col md={10}>
-                                            <FaSchool /> {data.schoolName}
+                                        <Col md={5}>
+                                            <FaUserCircle /> {data.firstName} {data.lastName}
+                                        </Col>
+                                        <Col md={5}>
+                                            {data.class}
                                         </Col>
                                         <Col md={1}>
                                             <Button size="sm" className="float-right" variant="warning" onClick={() => {
                                                 handleShow()
-                                                fetchSingleSchoolData(data.id)
+                                                fetchSingleStudentData(data.id)
                                             }}><FaEye /></Button>
                                         </Col>
                                         <Col md={1}>
@@ -196,57 +252,110 @@ export default function StudentList() {
 
                 </Card.Body>
             </Card>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleClose} size="lg">
                 <Modal.Header closeButton>
                     <Modal.Title>School Detail</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     {
                         spinDetail ? <center><Spinner /></center> :
-                            <Table hover>
-                                <tbody>
-                                    <tr>
+                            <Row>
+                                <Col>
+                                <h5>Student's Details:</h5>
+                                    <Table hover>
+                                        <tbody>
+                                            <tr>
+                                                <td><strong>Student Name</strong></td>
+                                                <td>{singleStudentData.firstName} {singleStudentData.firstName}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Father Name</strong></td>
+                                                <td>{singleStudentData.fatherName}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Mother Name</strong></td>
+                                                <td>{singleStudentData.motherName}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Date of Birth</strong></td>
+                                                <td>{singleStudentData.dob}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Gender</strong></td>
+                                                <td>{singleStudentData.gender}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Catagory</strong></td>
+                                                <td>{singleStudentData.catagory}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Email</strong></td>
+                                                <td>{singleStudentData.email}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Contact</strong></td>
+                                                <td>{singleStudentData.contact}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Class</strong></td>
+                                                <td>{singleStudentData.class}</td>
+                                            </tr>
+                                            <tr>
+                                                <td><strong>Address</strong></td>
+                                                <td>{singleStudentData.address}</td>
+                                            </tr>
+                                        </tbody>
+                                    </Table>
+                                </Col>
+                                <Col>
+                                <h5>Alloted School's Details:</h5>
+                                    <Table hover>
+                                        <tbody>
+                                        <tr>
                                         <td><strong>School Name</strong></td>
-                                        <td>{singleSchoolData.schoolName}</td>
+                                        <td>{schoolData.schoolName}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>School Code</strong></td>
-                                        <td>{singleSchoolData.schoolCode}</td>
+                                        <td>{schoolData.schoolCode}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>School Email</strong></td>
-                                        <td>{singleSchoolData.schoolEmail}</td>
+                                        <td>{schoolData.schoolEmail}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>School Contact</strong></td>
-                                        <td>{singleSchoolData.schoolContact}</td>
+                                        <td>{schoolData.schoolContact}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>School Address</strong></td>
-                                        <td>{singleSchoolData.schoolAddress}</td>
+                                        <td>{schoolData.schoolAddress}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>School Longitude</strong></td>
-                                        <td>{singleSchoolData.schoolLng}</td>
+                                        <td>{schoolData.schoolLng}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>School Latitude</strong></td>
-                                        <td>{singleSchoolData.schoolLat}</td>
+                                        <td>{schoolData.schoolLat}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>Head Name</strong></td>
-                                        <td>{singleSchoolData.headName}</td>
+                                        <td>{schoolData.headName}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>Head Email</strong></td>
-                                        <td>{singleSchoolData.headEmail}</td>
+                                        <td>{schoolData.headEmail}</td>
                                     </tr>
                                     <tr>
                                         <td><strong>Head Contact</strong></td>
-                                        <td>{singleSchoolData.headContact}</td>
+                                        <td>{schoolData.headContact}</td>
                                     </tr>
-                                </tbody>
-                            </Table>
+                                        </tbody>
+                                    </Table>
+                                </Col>
+                            </Row>
+
                     }
 
                 </Modal.Body>
